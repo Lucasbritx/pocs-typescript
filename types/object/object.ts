913aeace439ccb4afcf362ad08b05b441c905a74
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-17 15:24:16 -0300

    types obj obj

---
+++ types/object/object.ts
@@ -0,0 +1,9 @@
+/* https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#object-types */
+
+// The parameter's type annotation is an object type
+function printCoord(pt: { x: number; y: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
+printCoord({ x: 3, y: 7 });
