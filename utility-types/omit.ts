0dafc57c25b42b29dab5452ba0d6e072498ec891
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-19 10:21:40 -0300

    utility types omit

---
+++ utility-types/omit.ts
@@ -0,0 +1,23 @@
+/* https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys */
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: number;
+}
+
+type TodoPreview = Omit<Todo, 'description'>;
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+  createdAt: 1615544252770,
+};
+
+type TodoInfo = Omit<Todo, 'completed' | 'createdAt'>;
+
+const todoInfo: TodoInfo = {
+  title: 'Pick up kids',
+  description: 'Kindergarten closes at 5pm',
+};
