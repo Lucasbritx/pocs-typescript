73653ecf7fa9c23436587bcb5a99eb6240e40637
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-19 10:16:33 -0300

    utility types partial

---
+++ utility-types/partial.ts
@@ -0,0 +1,21 @@
+/* https://www.typescriptlang.org/docs/handbook/utility-types.html#partialtype */
+
+interface User {
+  name: string;
+  age: number;
+  email: string;
+}
+
+function createUser(user: Partial<User>): User {
+  return {
+    name: 'John Doe',
+    age: 30,
+    email: 'john.doe@example.com',
+    ...user,
+  };
+}
+
+const newUser = createUser({ name: 'Jane Doe' });
+
+console.log(newUser);
+// Output: { name: 'Jane Doe', age: 30, email: 'john.doe@example.com' }
