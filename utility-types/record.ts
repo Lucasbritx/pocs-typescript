920eff72336c32faaacd749231b4cacb207ce85e
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-19 21:33:14 -0300

    utility types record

---
+++ utility-types/record.ts
@@ -0,0 +1,14 @@
+/* https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type */
+
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+
+type CatName = 'miffy' | 'boris' | 'mordred';
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: 'Persian' },
+  boris: { age: 5, breed: 'Maine Coon' },
+  mordred: { age: 16, breed: 'British Shorthair' },
+};
