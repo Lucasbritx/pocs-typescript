487dfe599daaeff00ee1975dab0f5b3760420dc8
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-19 21:35:08 -0300

    utility types exclude

---
+++ utility-types/exclude.ts
@@ -0,0 +1,5 @@
+/* https://www.typescriptlang.org/docs/handbook/utility-types.html#excludeuniontype-excludedmembers */
+
+type T0 = Exclude<'a' | 'b' | 'c', 'a'>; // "b" | "c"
+type T1 = Exclude<'a' | 'b' | 'c', 'a' | 'b'>; // "c"
+type T2 = Exclude<string | number | (() => void), Function>; // string | number
