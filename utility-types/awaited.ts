8f67b0c3b886b12273f15810f3d4b44c97c89e9e
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-21 16:25:51 -0300

    type utilities awaited

---
+++ utility-types/awaited.ts
@@ -0,0 +1,10 @@
+/* https://www.typescriptlang.org/docs/handbook/utility-types.html#awaitedtype */
+
+type A = Awaited<Promise<string>>;
+// type A = string
+
+type B = Awaited<Promise<Promise<number>>>;
+// type B = number
+
+type C = Awaited<boolean | Promise<number>>;
+// type C = number | boolean
