e2cf07e1e288be10f2b8cd12f8af76a60e1c9f98
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-23 11:29:37 -0300

    type guards predicates

---
+++ type-guards/predicates.ts
@@ -0,0 +1,14 @@
+/* https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates */
+
+function isString(value: unknown): value is string {
+  return typeof value === 'string';
+}
+
+function example(x: unknown) {
+  if (isString(x)) {
+    // We can now call any 'string' method on 'x'.
+    x.toUpperCase();
+  } else {
+    console.log(x);
+  }
+}
