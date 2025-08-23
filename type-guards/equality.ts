2fcc9e042daf00065b6ce8621fa860b9a946ad68
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-23 11:19:03 -0300

    type guards equality

---
+++ type-guards/equality.ts
@@ -0,0 +1,12 @@
+/* https://www.typescriptlang.org/docs/handbook/2/narrowing.html#equality-narrowing */
+
+function example(x: string | number, y: string | boolean) {
+  if (x === y) {
+    // We can now call any 'string' method on 'x' or 'y'.
+    x.toUpperCase();
+    y.toLowerCase();
+  } else {
+    console.log(x);
+    console.log(y);
+  }
+}
