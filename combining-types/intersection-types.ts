7608e53deac358fd46fecfdee0e7910e62236e64
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-09-07 21:17:02 -0300

    combining types intersection

---
+++ combining-types/intersection-types.ts
@@ -0,0 +1,34 @@
+/* https://www.typescripttutorial.net/typescript-tutorial/typescript-intersection-types/ */
+
+interface BusinessPartner {
+    name: string;
+    credit: number;
+}
+
+interface Identity {
+    id: number;
+    name: string;
+}
+
+interface Contact {
+    email: string;
+    phone: string;
+}
+
+type Employee = Identity & Contact;
+
+let e: Employee = {
+    id: 100,
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phone: '(408)-897-5684'
+};
+
+type Customer = BusinessPartner & Contact;
+
+let c: Customer = {
+    name: 'ABC Inc.',
+    credit: 1000000,
+    email: 'sales@abcinc.com',
+    phone: '(408)-897-5735'
+};
